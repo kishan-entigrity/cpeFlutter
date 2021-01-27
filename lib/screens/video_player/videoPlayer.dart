665243deb99facfe23c3b9af9735fc31f63a0a39
kishansh31 <55207490+kishansh31@users.noreply.github.com>
2021-01-27 21:10:54 +0530

    Commit with following things:
    - Worked on managing the states of videoPlayer.dart,
    - Worked on the timer class with despose method on every 5 seconds of interval.

--- lib/screens/video_player/videoPlayer.dart
+++ lib/screens/video_player/videoPlayer.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cpe_flutter/components/TopBar.dart';
 import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/cupertino.dart';
@@ -17,6 +19,7 @@ class _VideoPlayerState extends State<VideoPlayerFlickker> {
   final webDetailsObj;
 
   FlickManager flickManager;
+  Timer _timer;
 
   @override
   void initState() {
@@ -27,11 +30,14 @@ class _VideoPlayerState extends State<VideoPlayerFlickker> {
     flickManager = FlickManager(
       videoPlayerController: VideoPlayerController.network(videoUrl),
     );
+
+    // startBasicTimer();
   }
 
   @override
   void dispose() {
     flickManager.dispose();
+    _timer.cancel();
     super.dispose();
   }
 
@@ -48,11 +54,53 @@ class _VideoPlayerState extends State<VideoPlayerFlickker> {
               width: double.infinity,
               height: 180.0,
               // height: SDP.sdp(300),
-              child: FlickVideoPlayer(flickManager: flickManager),
+              child: FlickVideoPlayer(
+                flickManager: flickManager,
+                /*flickVideoWithControls: FlickPlayToggle(
+                  // togglePlay: flickControlManager.pause(),
+                  pauseChild: flickManager.flickControlManager.pause(),
+                ),*/
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print('onTap for TogglePlay button is called..');
+                flickManager.flickControlManager.togglePlay();
+              },
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  'TogglePlayController',
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                bool state = flickManager.flickVideoManager.isPlaying;
+                // bool bufferState = flickManager.flickVideoManager.addListener(() {flickManager.flickVideoManager.});
+                if (flickManager.flickVideoManager.isPlaying) {
+                  print('Video player is playing $state');
+                } else {
+                  print('Video player is on pause state $state');
+                }
+              },
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  'Check State',
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void startBasicTimer() {
+    _timer = new Timer.periodic(Duration(seconds: 5), (timer) {
+      var timer = DateTime.now();
+      print('Start Basic Timer is called.. $timer');
+    });
   }
 }
