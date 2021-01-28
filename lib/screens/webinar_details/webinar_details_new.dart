7a678e6bf57e11ce7a28e85073aa2fc182c22de2
kishansh31 <55207490+kishansh31@users.noreply.github.com>
2021-01-28 21:18:38 +0530

    Commit with following things:
    - Worked on adding stopBasicTimer on the onDespose method.

--- lib/screens/webinar_details/webinar_details_new.dart
+++ lib/screens/webinar_details/webinar_details_new.dart
@@ -106,6 +106,7 @@ class _WebinarDetailsNewState extends State<WebinarDetailsNew> {
   void dispose() {
     flickManager.dispose();
     _timer.cancel();
+    stopBasicTimer();
     super.dispose();
   }
 
