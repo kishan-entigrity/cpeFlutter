5292fb0b6855e69084b1f5539c00719b42df2e4c
kishansh31 <55207490+kishansh31@users.noreply.github.com>
2021-04-14 13:45:16 +0530

    - Worked on the issue for the text lenght overflow on the childCardCompany.dart data.

--- lib/screens/webinar_details/childCardCompany.dart
+++ lib/screens/webinar_details/childCardCompany.dart
@@ -21,43 +21,47 @@ class childCardCompany extends StatelessWidget {
                 radius: 10.5.w,
                 backgroundImage: NetworkImage(presenterObj['company_logo']),
               ),
-              Padding(
-                padding: EdgeInsets.only(
-                  left: 10.0,
-                  right: 10.0,
-                  top: 5.0,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      presenterObj['company_name'],
-                      maxLines: 5,
-                      softWrap: true,
-                      style: TextStyle(
-                        fontFamily: 'Whitney Bold',
-                        fontSize: 13.5.sp,
-                        color: Colors.black,
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: 10.0,
+                    right: 10.0,
+                    top: 5.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          presenterObj['company_name'],
+                          // 'hello world hello world hello world hello world hello world ',
+                          maxLines: 5,
+                          // softWrap: true,
+                          style: TextStyle(
+                            fontFamily: 'Whitney Bold',
+                            fontSize: 13.5.sp,
+                            color: Colors.black,
+                          ),
+                        ),
                       ),
-                    ),
-                    Text(
-                      presenterObj['company_website'],
-                      style: TextStyle(
-                        fontFamily: 'Whitney Medium',
-                        fontSize: 13.0.sp,
-                        color: Colors.black,
+                      Text(
+                        presenterObj['company_website'],
+                        style: TextStyle(
+                          fontFamily: 'Whitney Medium',
+                          fontSize: 13.0.sp,
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ],
           ),
         ),
         Container(
-          padding:
-              EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 10.0),
+          padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 10.0),
           margin: EdgeInsets.only(left: 10.0, right: 10.0),
           width: double.infinity,
           child: Html(
