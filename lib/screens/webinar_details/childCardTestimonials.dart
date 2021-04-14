2af2bfc5074a46c5b2ffb1297b7daf1fd13d36ea
kishansh31 <55207490+kishansh31@users.noreply.github.com>
2021-04-14 18:05:02 +0530

    - Worked on the issue for the testimonials for the showing error message if we are not getting testimonials on details api.

--- lib/screens/webinar_details/childCardTestimonials.dart
+++ lib/screens/webinar_details/childCardTestimonials.dart
@@ -12,6 +12,8 @@ class childCardTestimonials extends StatefulWidget {
   final respTestimonials;
   final String webinarId;
 
+  // final int lengthTestimonials;
+
   @override
   _childCardTestimonialsState createState() => _childCardTestimonialsState(respTestimonials, webinarId);
 }
@@ -22,48 +24,72 @@ class _childCardTestimonialsState extends State<childCardTestimonials> {
   final respTestimonials;
   final String webinarId;
 
+  // final int lengthTestimonials;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    // print('Length For respTestimonials is : $lengthTestimonials');
+    print('Init formtestimonials is called length is : ${respTestimonials.length}');
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       /*child: Text(
         strDetails,
       ),*/
-      child: Column(
-        children: <Widget>[
-          TestimonialDataCell(respTestimonials[0]['first_name'] + ' ' + respTestimonials[0]['last_name'] + respTestimonials[0]['designation'],
-              respTestimonials[0]['date'], respTestimonials[0]['rate'], respTestimonials[0]['review']),
-          Container(
-            width: double.infinity,
-            height: 0.5,
-            color: Colors.black,
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          TestimonialDataCell(respTestimonials[1]['first_name'] + ' ' + respTestimonials[1]['last_name'] + respTestimonials[1]['designation'],
-              respTestimonials[1]['date'], respTestimonials[1]['rate'], respTestimonials[1]['review']),
-          GestureDetector(
-            // Have to put Navigator call for Testimonials screen from here..
-            // Have to pass the data of webinar ID..
-            onTap: () {
-              print('Clicked on view more button');
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Testimonials(webinarId),
+      child: respTestimonials.length.toString() == '0'
+          ? Container(
+              height: 20.0.w,
+              // color: Colors.teal,
+              child: Center(
+                child: Text(
+                  'No testimoials available for this webinar',
+                  style: TextStyle(
+                    fontSize: 10.0.sp,
+                    fontFamily: 'Whitney Medium',
+                  ),
                 ),
-              );
-            },
-            child: Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                'View More',
-                style: kUserDataBlueTestimonials,
               ),
+            )
+          : Column(
+              children: <Widget>[
+                TestimonialDataCell(respTestimonials[0]['first_name'] + ' ' + respTestimonials[0]['last_name'] + respTestimonials[0]['designation'],
+                    respTestimonials[0]['date'], respTestimonials[0]['rate'], respTestimonials[0]['review']),
+                Container(
+                  width: double.infinity,
+                  height: 0.5,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TestimonialDataCell(respTestimonials[1]['first_name'] + ' ' + respTestimonials[1]['last_name'] + respTestimonials[1]['designation'],
+                    respTestimonials[1]['date'], respTestimonials[1]['rate'], respTestimonials[1]['review']),
+                GestureDetector(
+                  // Have to put Navigator call for Testimonials screen from here..
+                  // Have to pass the data of webinar ID..
+                  onTap: () {
+                    print('Clicked on view more button');
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Testimonials(webinarId),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'View More',
+                      style: kUserDataBlueTestimonials,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 }
